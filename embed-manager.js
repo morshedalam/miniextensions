62a90ada35cd6238c0a302db1a8638f491f55292
Morshed Alam <morshed201@gmail.com>
2021-02-20 17:09:12 +0600

    Applied url params precedence over configurations

--- embed-manager.js
+++ embed-manager.js
@@ -5,24 +5,37 @@ window.MiniExtensionFormManager = window.MiniExtensionFormManager || new functio
                 var form_key = params.key;
                 var api_url = 'https://api.' + host + '/v1/iframe-embed/' + form_key + '.js?absoluteShareUrl=';
                 var form_url = 'https://app.' + host + '/form/' + form_key;
+                var url_params = {};
+                var form_param = '';
+
+                if (params.precedenceUrlParams === undefined)
+                    params.precedenceUrlParams = false;
+                else if (typeof(params.precedenceUrlParams) !== "boolean")
+                    params.precedenceUrlParams = false;
+
+                if (params.precedenceUrlParams || params.prePopulate == window.location) {
+                    url_params = MiniExtensionFormManager.getQueryParams();
+                }
+
+                if (typeof(params.prePopulate) != 'object') {
+                    params.prePopulate = {};
+                }
+
+                if (params.precedenceUrlParams) {
+                    for (var key in url_params) {
+                        params.prePopulate[key] = url_params[key];
+                    }
+                }
 
                 if (params.prePopulate) {
-                    if (params.prePopulate == window.location) {
-                        params.prePopulate = MiniExtensionFormManager.getQueryParams();
-                    } else if (typeof(params.prePopulate) != 'object') {
-                        params.prePopulate = {}
-                    }
-
                     var esc = encodeURIComponent;
-                    var form_param = Object.keys(params.prePopulate)
+                    form_param = Object.keys(params.prePopulate)
                         .map(function (k) {
                             return esc(k) + '=' + esc(params.prePopulate[k]);
                         }).join('&');
                 }
 
-                var script_url = api_url + form_url + (form_param ? '?' + form_param : '');
-
-                MiniExtensionFormManager.insertFrame(form_key, script_url);
+                MiniExtensionFormManager.insertFrame(form_key, (api_url + form_url + (form_param ? '?' + form_param : '')));
             },
             getQueryParam: function (name) {
                 var val = MiniExtensionFormManager.getQueryParams()[name];
@@ -36,7 +49,8 @@ window.MiniExtensionFormManager = window.MiniExtensionFormManager || new functio
                     query = query.split("&");
                     for (var i = 0; i < query.length; i++) {
                         tmp = query[i].split("=");
-                        params[tmp[0]] = tmp[1];
+                        if ((tmp[0] + '').lastIndexOf('prefill_', 0) === 0)
+                            params[tmp[0]] = tmp[1];
                     }
                 }
 
